31f270760432e1cc2a25774d8b478712a51f3d9d
Saul Gomes <klowd.bk@gmail.com>
2023-01-11 09:24:37 -0300

    hotfix: jestConfig

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,13 @@
-export default {
+module.exports = {
   coverageProvider: "v8",
   preset: "ts-jest",
-  testMatch: ["**/__tests__/integration/**/*.[jt]s?(x)"],
+  testMatch: ["**/__tests__/integrations/**/*.[jt]s?(x)"],
+  roots: ["<rootDir>/src"],
+  collectCoverageFrom: ["<rootDir>/src/*/.ts"],
+  coverageDirectory: "coverage",
+  testEnvironment: "node",
+  transform: { ".+.ts$": "ts-jest" },
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+  },
 };
