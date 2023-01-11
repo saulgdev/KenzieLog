2682b1f527a2a74bca2eb03140dd9deab0b15ce7
Gabriel-Malafaia <gabrielbsnsp@gmail.com>
2023-01-11 09:22:56 -0300

    feat: inserting user router pattern

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+const userRoutes = Router();
+
+userRoutes.post("");
+userRoutes.get("");
+userRoutes.get("/:id");
+userRoutes.get("/:id/requests");
+userRoutes.patch("");
+userRoutes.delete("");
+
+export default userRoutes;
