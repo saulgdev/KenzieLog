9c2c468f99a9238c17083d82a471d753a1b7efb2
Pedro Henrique Feitosa <50965091+pedrofeitosa97@users.noreply.github.com>
2023-01-12 14:07:06 -0300

    feat: select user without password

--- src/services/users/listUsers.service.ts
+++ src/services/users/listUsers.service.ts
@@ -3,7 +3,17 @@ import { Users } from "../../entities/users.entitiy"
 
 const listUsersService = async () => {
     const dataRepository = AppDataSource.getRepository(Users)
-    const users = await dataRepository.find()
+    const users = await dataRepository.find({
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            isAdm: true,
+            isActive: true,
+            createdAt: true,
+            updatedAt: true
+        },
+    })
     return users
 }
 
