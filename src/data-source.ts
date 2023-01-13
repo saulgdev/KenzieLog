36263832baa0ef29a9eb8afc2e9390d46865de9a
Saul Gomes <klowd.bk@gmail.com>
2023-01-13 13:43:46 -0300

    config deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -10,6 +10,13 @@ const AppDataSource = new DataSource(
         synchronize: true,
         entities: ["src/entities/*.ts"],
       }
+    : process.env.NODE_ENV === "production"
+    ? {
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        entities: [path.join(__dirname, "./entities/**.{js,ts}")],
+        migrations: [path.join(__dirname, "./migrations/**.{js,ts}")],
+      }
     : {
         type: "postgres",
         host: process.env.PGHOST,
